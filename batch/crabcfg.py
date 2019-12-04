81648f1e32e72abb94fe75f0164a36527d0f24f2
Nick Amin <amin.nj@gmail.com>
2019-12-04 13:58:05 -0800

    bool for unblinded 1ifb

--- batch/crabcfg.py
+++ batch/crabcfg.py
@@ -3,9 +3,13 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 # https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3ConfigurationFile
 config = config()
 
-data_era = "D"
-ntuple_version = "v4"
-do_2018C_1fb_unblind = False
+# data_era = "D"
+# ntuple_version = "v4"
+# do_2018C_1fb_unblind = False
+
+data_era = "C"
+ntuple_version = "v8"
+do_2018C_1fb_unblind = True
 
 config.General.requestName = 'skim_2018{}_{}'.format(data_era,ntuple_version)
 config.Data.inputDataset = '/ScoutingCaloMuon/Run2018{}-v1/RAW'.format(data_era)
@@ -18,6 +22,9 @@ config.JobType.psetName = 'Scouting/NtupleMaker/test/dataproducer.py'
 
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = int(15e6)
+
+if do_2018C_1fb_unblind:
+    config.Data.lumiMask = "data/unblind_2018C_1fb_JSON.txt"
 
 config.Data.publication = False
 config.Site.storageSite = "T2_US_UCSD"
