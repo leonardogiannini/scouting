c60a374c2539d675123f8e4a8a84345e96243d38
Nick Amin <amin.nj@gmail.com>
2019-12-13 19:28:36 -0800

    clean up

--- batch/submit_baby_jobs.py
+++ batch/submit_baby_jobs.py
@@ -16,68 +16,42 @@ blacklisted_machines = [
 if blacklisted_machines:
     extra_requirements = " && ".join(map(lambda x: '(TARGET.Machine != "{0}")'.format(x),blacklisted_machines))
 
-
-def get_data_tasks(info):
+def get_tasks(infos):
     tasks = []
-    for reqname,tag,extra_args in info:
-        task = CondorTask(
-                sample = DirectorySample(
-                    location = "/hadoop/cms/store/user/namin/ScoutingCaloMuon/crab_{}/*/*/".format(reqname),
-                    dataset = "/ScoutingCaloMuon/Run2018{}/RAW".format(reqname)
-                    ),
-                output_name = "output.root",
-                executable = "executables/scouting_exe.sh",
-                tarfile = "package.tar.gz",
-                MB_per_output = (1000 if "skim1cm" in extra_args else 4000),
-                condor_submit_params = {
-                    "sites":"T2_US_UCSD",
-                    "classads": [
-                        ["metis_extraargs",extra_args],
-                        ["JobBatchName",reqname],
-                        ],
-                    "requirements_line": 'Requirements = ((HAS_SINGULARITY=?=True) && (HAS_CVMFS_cms_cern_ch =?= true) && {extra_requirements})'.format(extra_requirements=extra_requirements),
-                    },
-                cmssw_version = "CMSSW_10_2_5",
-                scram_arch = "slc6_amd64_gcc700",
-                tag = tag,
-                )
-        tasks.append(task)
-    return tasks
-
-def get_signal_tasks(pattern,tag,extra_args="",func_dataset=None):
-    samples = []
-    for location in glob.glob(pattern):
-        if func_dataset is not None:
-            dataset = func_dataset(location)
+    for info in infos:
+        location = info["location"]
+        dataset = info["dataset"]
+        isdata = info["isdata"]
+        tag = info["tag"]
+        extra_args = info.get("extra_args","")
+        kwargs = {}
+        if isdata:
+            kwargs["MB_per_output"] = (4000 if "skim1cm" in extra_args else 1000)
+            batchname = dataset.split("_",1)[-1].split("/")[0]+"_"+tag
         else:
-            taskname = location.rstrip("/").rsplit("/")[-1]
-            dataset = "/{}/{}/BABY".format(
-                    taskname.split("_",1)[0],
-                    taskname.split("_",1)[1].split("_RAWSIM")[0],
-                    )
-        samples.append(DirectorySample(location=location, dataset=dataset))
-    tasks = []
-    for sample in samples:
+            kwargs["files_per_output"] = 1e5
+            batchname = "_".join(location.split("params_",1)[1].split("_")[:2] + [tag])
         task = CondorTask(
-                sample = sample,
+                sample = DirectorySample(location=location,dataset=dataset),
                 output_name = "output.root",
                 executable = "executables/scouting_exe.sh",
                 tarfile = "package.tar.gz",
-                files_per_output = int(1e5),
                 condor_submit_params = {
                     "sites":"T2_US_UCSD",
                     "classads": [
                         ["metis_extraargs",extra_args],
-                        ["JobBatchName",sample.get_datasetname().split("/")[2].replace("params_","")],
+                        ["JobBatchName",batchname],
                         ],
                     "requirements_line": 'Requirements = ((HAS_SINGULARITY=?=True) && (HAS_CVMFS_cms_cern_ch =?= true) && {extra_requirements})'.format(extra_requirements=extra_requirements),
                     },
                 cmssw_version = "CMSSW_10_2_5",
                 scram_arch = "slc6_amd64_gcc700",
                 tag = tag,
+                **kwargs
                 )
         tasks.append(task)
     return tasks
+
 
 def get_bg_tasks(tag,extra_args=""):
     samples = [
@@ -113,50 +87,41 @@ if __name__ == "__main__":
     # print("Did you do `./make_tar.sh`? Sleeping for 5s for you to quit if not.")
     # time.sleep(5)
 
-    # Entries of [CRAB request name, output baby tag, extra string passed to babymaker]
-    data_info = [
-            # ["skim_2018A_v4","v4", ""],
-            # ["skim_2018B_v4","v4", ""],
-            # ["skim_2018C_v4","v4", ""],
-            # ["skim_2018D_v4","v4", ""],
+    # each task needs 
+    # - a location (path to input root files)
+    # - an output dataset name (just for bookkeping/output folder naming)
+    # - a tag (for bookkeeping/output folder naming/versioning)
+    # - isdata=[True|False]
+    # extra parameters to the babymaker can be passed with extra_args.
+    infos = []
 
-            # ["skim_2018C_v4_unblind1fb","v4", ""],
-            # ["skim_2018C_v4_unblind1fb","vtestskim1cm", "--skim1cm"],
+    # DATA
+    infos.extend([
+            dict(location="/hadoop/cms/store/user/namin/ScoutingCaloMuon/crab_skim_2018C_v9_unblind1fb/*/*/", dataset="/ScoutingCaloMuon/Run2018skim_2018C_v9_unblind1fb/RAW", tag="v10", isdata=True, extra_args=""),
+            dict(location="/hadoop/cms/store/user/namin/ScoutingCaloMuon/crab_skim_2018C_v9_unblind1fb/*/*/", dataset="/ScoutingCaloMuon/Run2018skim_2018C_v9_unblind1fb/RAW", tag="v10skim1cm", isdata=True, extra_args="--skim1cm"),
+            ])
 
-            ["skim_2018C_v4_unblind1fb","v7jets", ""],
-            ["skim_2018C_v4_unblind1fb","v7skim1cmjets", "--skim1cm"],
-            ["skim_2018C_v4","v7skim1cmjets", "--skim1cm"],
+    # MC
+    locations = glob.glob("/hadoop/cms/store/user/namin/ProjectMetis/HToZdZdTo2Mu2X_params_mzd20_ctau50mm_RAWSIM_v9/")
+    for location in locations:
+        taskname = location.rstrip("/").rsplit("/")[-1]
+        dataset = "/{}/{}/BABY".format(
+                taskname.split("_",1)[0],
+                taskname.split("_",1)[1].split("_RAWSIM")[0],
+                )
+        infos.append(dict(location=location, dataset=dataset, isdata=False, tag="v10"))
+        
+    # pprint(infos,width=200)
 
-            ]
-    data_tasks = get_data_tasks(data_info)
+    tasks = get_tasks(infos)
 
-    signal_tasks = []
-
-    signal_tasks = get_signal_tasks(
-            pattern="/hadoop/cms/store/user/namin/ProjectMetis/HToZdZdTo2Mu2X_params_mzd*_ctau*mm_RAWSIM_v1/",
-            tag="v7",
-            extra_args="--allevents",
-            )
-
-    # /hadoop/cms/store/user/namin/DisplacedMuons/2018/GluGluTo0PHH125ToZprimeZprimeTo2Mu2X_CTauVprime_50mm/PREMIX-RAWSIM/
-
-    # signal_tasks = get_signal_tasks(
-    #         pattern = "/hadoop/cms/store/user/namin/DisplacedMuons/2018/GluGluTo0PHH125ToZprimeZprimeTo2Mu2X_CTauVprime_50mm/PREMIX-RAWSIM/",
-    #         func_dataset = (lambda x:"/HToZdZdTo2Mu2X_250kevts/params_mzd20_ctau50mm/BABY"),
-    #         tag="v5jets",
-    #         extra_args="--allevents",
-    #         )
-    # print(signal_tasks)
-
-    bg_tasks = []
-
-    bg_tasks = get_bg_tasks(
-            tag="v7",
-            )
+    # tasks.extend(get_bg_tasks(
+    #         tag="v7",
+    #         ))
 
     for _ in range(500):
         total_summary = {}
-        for task in data_tasks + signal_tasks + bg_tasks:
+        for task in tasks:
             task.process()
             total_summary[task.get_sample().get_datasetname()] = task.get_task_summary()
         StatsParser(data=total_summary, webdir="~/public_html/dump/scouting/").do()
