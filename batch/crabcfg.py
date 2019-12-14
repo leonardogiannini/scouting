fb1f52680dfbb5ebece75f71dc7fc9ae67d511ca
Nick Amin <amin.nj@gmail.com>
2019-12-13 16:50:57 -0800

    update

--- batch/crabcfg.py
+++ batch/crabcfg.py
@@ -7,11 +7,19 @@ config = config()
 # ntuple_version = "v4"
 # do_2018C_1fb_unblind = False
 
-data_era = "C"
-ntuple_version = "v8"
-do_2018C_1fb_unblind = True
+# data_era = "C"
+# ntuple_version = "v9"
+# do_2018C_1fb_unblind = True
 
-config.General.requestName = 'skim_2018{}_{}'.format(data_era,ntuple_version)
+data_era = "C"
+ntuple_version = "v9"
+do_2018C_1fb_unblind = False
+
+config.General.requestName = 'skim_2018{}_{}{}'.format(
+        data_era,
+        ntuple_version,
+        ("_unblind1fb" if do_2018C_1fb_unblind else "")
+        )
 config.Data.inputDataset = '/ScoutingCaloMuon/Run2018{}-v1/RAW'.format(data_era)
 
 config.General.workArea = 'crab'
@@ -21,7 +29,7 @@ config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'Scouting/NtupleMaker/test/dataproducer.py'
 
 config.Data.splitting = 'EventAwareLumiBased'
-config.Data.unitsPerJob = int(15e6)
+config.Data.unitsPerJob = int(10e6)
 
 if do_2018C_1fb_unblind:
     config.Data.lumiMask = "data/unblind_2018C_1fb_JSON.txt"
